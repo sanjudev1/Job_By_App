8343fa7d0b939991923a891a9138cd94edf58c30
sanjudev1 <sanjeevch.131@gmail.com>
2022-10-26 10:30:45 +0000

    Job By App

---
+++ src/components/Home/index.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line no-unused-vars
+import {Link, Redirect} from 'react-router-dom'
+import Header from '../Header'
+import './index.css'
+
+const Home = props => {
+  // eslint-disable-next-line no-unused-vars
+  const onRedirectToJobs = () => {
+    const {history} = props
+    history.replace('/jobs')
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="home-container">
+        <h1 className="home-heading">
+          Find The Job That <br />
+          Fits Your Life
+        </h1>
+        <p className="home-paragraph">
+          Millions of people are searching for jobs, salary information, company
+          reviews. Find the job that fits your abilities and potential.
+        </p>
+        <Link className="retry-btn-link" to="/jobs">
+          <button className="home-jobs-button" type="button">
+            Jobs
+          </button>
+        </Link>
+      </div>
+    </>
+  )
+}
+
+export default Home
